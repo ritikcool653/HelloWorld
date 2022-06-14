e7083c87212c4c09b789984cba04526c2b504512
Verma <ritik.verma@mind.motherson.com>
2022-06-14 09:09:41 +0530

    Hello

---
+++ src/main/java/MTSL/Hello/App.java
@@ -0,0 +1,13 @@
+package MTSL.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
